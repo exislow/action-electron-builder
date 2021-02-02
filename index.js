2c1c40fbec248de0d75c7d1550981bda3480fd7e
exislow <exislow@users.noreply.github.com>
2021-02-02 18:27:15 +0100

    Hacky modification for quasar
    Replaced yarn/npm run with quasar build command.

--- index.js
+++ index.js
@@ -130,9 +130,10 @@ const runAction = () => {
 	for (let i = 0; i < maxAttempts; i += 1) {
 		try {
 			run(
-				`${useNpm ? "npx --no-install" : "yarn run"} ${cmd} --${platform} ${
+				/*`${useNpm ? "npx --no-install" : "yarn run"} ${cmd} --${platform} ${
 					release ? "--publish always" : ""
-				} ${args}`,
+				} ${args}`*/
+				`quasar build -m electron`,
 				appRoot,
 			);
 			break;
